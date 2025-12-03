136e513e8863f75111aa0d9fa82e4aa7aeae38f5
Luiz Gustavo <166750090+iTzGustavo-Tech@users.noreply.github.com>
2025-12-03 01:51:17 +0000

    primeira Carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,38 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado1, codigo1[4], nomecid1[30]; // Estado, codigo da cidade e nome da cidade.
+    int populacao1, pontost1;  // População e pontos turisticos.
+    float area1, pib1; // Área em km2 e Produto interno bruto.
+
+    char estado2, codigo2[4], nomecid2[30]; // Estado, codigo da cidade e nome da cidade.
+    int populacao2, pontost2;  // População e pontos turisticos.
+    float area2, pib2; // Área em km2 e Produto interno bruto
 
   // Área para entrada de dados
+
+    printf("=== Carta 1 ===\n");
+    printf("Estado A a H: ");
+    scanf("%c", &estado1);
+
+    printf("Código da Carta A01 a A08: ");
+    scanf("%s", &codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", &nomecid1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos Tusrísticos: ");
+    scanf("%f", &pontost1);
+
 
   // Área para exibição dos dados da cidade
 
